7c8e524960e14a14f2932245b9794e13f82d0702
Joseph Epherson <josephepherson@Josephs-MacBook-Air.local>
2021-02-17 12:55:46 -0500

    server

--- backend/server.js
+++ backend/server.js
@@ -4,4 +4,4 @@ const app = express();
 
 const port = 5001;
 
-app.listen(port, () => console.log(`Server started on port ${port}`))
+app.listen(port, () => console.log(`Server started on port ${port}`))  
--- src/actions/constants.js
+++ src/actions/constants.js
@@ -1,5 +1,5 @@
 // Sets up constant action types in one location that can be exported though out the applaction. 
 export const actionTypes = {
     ADD_A_TEST = "ADD_A_TEST",
-    ADD_A_USER = "ADD_A_USER"
+    ADD_A_USER = "ADD_A_USER",
 }
