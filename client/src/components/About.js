b6188d68d06ab22673c32636cbdbbcfdec472a32
MatthewLee4 <67612225+MatthewLee4@users.noreply.github.com>
2021-02-27 12:00:48 -0600

    updated the navbar for the about us page

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -1,29 +1,27 @@
 import React from 'react';
-import AppBar from '@material-ui/core/AppBar';
 import Button from '@material-ui/core/Button';
-// import CameraIcon from '@material-ui/icons/PhotoCamera';
 import Card from '@material-ui/core/Card';
 import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
-import CssBaseline from '@material-ui/core/CssBaseline';
 import Grid from '@material-ui/core/Grid';
-import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
 import Link from '@material-ui/core/Link';
+import MenuAppBar from './Header'
 
 function Copyright() {
   return (
+    <>
     <Typography variant="body2" color="textSecondary" align="center">
       {'Copyright © '}
       <Link color="inherit" href="https://material-ui.com/">
-        {/* Your Website */}
       </Link>{' '}
       {new Date().getFullYear()}
       {'.'}
     </Typography>
+    </>
   );
 }
 
@@ -68,16 +66,8 @@ export default function Album() {
   const classes = useStyles();
 
   return (
-    <React.Fragment>
-      <CssBaseline />
-      <AppBar position="relative">
-        <Toolbar>
-          {/* <CameraIcon className={classes.icon} /> */}
-          <Typography variant="h6" color="inherit" noWrap>
-            Certified
-          </Typography>
-        </Toolbar>
-      </AppBar>
+    <>
+    <MenuAppBar />
       <main>
         {/* Hero unit */}
         <div className={classes.heroContent}>
@@ -245,7 +235,7 @@ export default function Album() {
         <Copyright />
       </footer>
       {/* End footer */}
-    </React.Fragment>
+    </>
   );
 }
 
