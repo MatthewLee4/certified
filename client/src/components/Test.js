3f0e1929e2065219ad9c71dde1bbf55ef0905dfc
nastrodamous <andre@Prongs.localdomain>
2021-02-27 13:55:55 -0500

    updated

--- client/src/components/Test.js
+++ client/src/components/Test.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import MenuAppBar from './Header'
+import Card from '@material-ui/core/Card';
+const axios = require('axios');
 
 const Test = () => {
     return (
@@ -12,4 +14,27 @@ const Test = () => {
     )
 };
 
+fetchTest = (props) => {
+    axios.get("https://opentdb.com/api.php?amount=10&category=21&difficulty=medium&type=multiple")
+    .then(
+        async(response) => {
+           mapResponses(response.data)
+       }
+    )
+}
+
+
 export default Test;
+
+// {
+//     "category": "Sports",
+//     "type": "multiple",
+//     "difficulty": "medium",
+//     "question": "At which bridge does the annual Oxford and Cambridge boat race start?",
+//     "correct_answer": "Putney",
+//     "incorrect_answers": [
+//         "Hammersmith",
+//         "Vauxhall ",
+//         "Battersea"
+//     ]
+// }
--- client/src/store.js
+++ client/src/store.js
@@ -4,9 +4,15 @@ import { createStore, applyMiddleware } from "redux";
 import thunk from "redux-thunk"
 
 
-// const initState = {
-//     test: [],
-//     users: []
-// }
+const initState = {
+    test: {},
+    users: {},
+    score: 0,
+    testAnswer: "",
+    userSubmit: "",
+    timer: 0
+}
 
 export const store = createStore( reducers, applyMiddleware( thunk ));
+
+//Need to get 7 out of 10 questions, then need to store score in database to display on card
